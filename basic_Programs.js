5f82c33ce530873fa3c098a1bed42a25fcf0e3f6
Aditi Pujari <pujariaditi11@gmail.com>
2026-01-13 23:54:24 +0530

    start the web technology

---
+++ basic_Programs.js
@@ -0,0 +1,35 @@
+//variables
+
+//1
+var a =10;
+let b =100;
+console.log(a,b);
+a = 20;
+b = 200
+console.log(a,b);
+
+//scope 
+var a =10;
+let ab =20;
+{
+    var a =10;
+    let ab = 200;
+    console.log("a : ",a ,"ab : ",ab);
+}
+console.log("a : ",a ,"ab : ",ab);
+
+//const
+const x =1;
+//x = 20; ----error
+console.log(x)
+
+
+// == , ===    (checking )
+let m = 20;
+var n = 20;
+if(m == n){
+    console.log("condition check");
+}
+if(m === n){
+    console.log("condition and data type both check");
+}
