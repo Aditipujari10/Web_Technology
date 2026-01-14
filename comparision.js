af8328acca6b85ea6b81adaab433888f3b3ba7be
Aditi Pujari <pujariaditi11@gmail.com>
2026-01-14 15:34:50 +0530

    comparision file added

---
+++ comparision.js
@@ -0,0 +1,25 @@
+console.log(2>1);
+console.log(3<2);
+console.log(1>=6);
+console.log(7<=9);
+console.log(2==6);
+console.log(2!=0);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined <= 0);
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+//activity
+//check null and undefined
