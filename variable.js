02be281d858aebf478e185817c38ccfa9decf775
Aditi Pujari <pujariaditi11@gmail.com>
2026-01-14 15:35:30 +0530

    variable file addded

---
+++ variable.js
@@ -0,0 +1,65 @@
+//conversion 
+//1
+let num = 33;
+let string = String(num);
+console.log(string);
+console.log(typeof(string));
+console.log(typeof(num));
+
+
+//2
+let num1 = "10";
+let num2 = Number(num1);
+console.log(num2);
+console.log(typeof(num2)); 
+
+
+
+//3 
+let num3 = "true";
+let num4 = 
+
+
+// let jj = "33";
+// let str = Number(jj);
+// console.log(typeof(str));
+
+
+//activity
+//conversion
+
+
+//Arithmatic operations 
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2%2);
+
+//string concatination
+let str1 = "Hello";
+let name = " Aditi";
+console.log(str1 + name);
+
+//note ---->
+//javascript only automatically convert a string to a numbers in arithmatical operations
+console.log("1" + 2);
+console.log("1" + 2 +2);
+console.log(1 + "2");
+console.log(1+2+"2");
+
+//activity 2
+//deep study
+console.log(true);
+console.log(+true);
+console.log(+ "");
+
+//increament
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+let scoreCounter = 200;
+++scoreCounter;
+console.log(scoreCounter);
+
